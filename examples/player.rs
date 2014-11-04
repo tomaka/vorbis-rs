2ab989f82b0399b1f52d8535a8dadb164cb31183
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-11-04 18:04:58 +0100

    Bugfixes and adjustements in player example

--- examples/player.rs
+++ examples/player.rs
@@ -1,3 +1,5 @@
+#![feature(tuple_indexing)]
+
 extern crate vorbis;
 extern crate openal;
 extern crate time;
@@ -16,46 +18,81 @@ fn main() {
 
     let source = al::Source::gen();
 
+    // creating the decoder
     let mut decoder = vorbis::Decoder::new(BufReader::new(include_bin!("mozart_symfony_40.ogg")))
         .unwrap();
 
+    // storing a list of tuples
+    // - the first element is the time when the buffer will finish playing
+    // - the second element is the buffer
     let mut buffers = RingBuf::new();
-    let mut packet_num = 0u;
-    let mut previous_packet_duration = Duration::milliseconds(0);
 
+    // simple counter that we println!, doesn't have any usage
+    let mut packet_num = 0u;
+
+    // looping through the packets
     for packet in decoder.packets() {
+        packet_num += 1;
+
+        // the iterator produces Result<Packet, VorbisError> objects, so we need to unwrap
         let packet = packet.unwrap();
 
-        packet_num += 1;
-        if packet_num >= 2 {
+        // removing from the list of buffers all the buffers that should have stopped playing
+        let now = time::precise_time_ns();
+        while buffers.front().map(|&(stop, _)| stop).unwrap_or(now) < now {
             buffers.pop_front();
         }
 
-        buffers.push(al::Buffer::gen());
+        // calculating when the current packet will finish playing
+        let packet_duration = packet.data.len() as f32 /
+                              (packet.channels as u64 * packet.rate) as f32;
+        let packet_play_finish = buffers.back().map(|&(stop, _)| stop).unwrap_or(now) +
+                                 (packet_duration * 1000000000.0) as u64;
 
-        let packet_duration = packet.data.len() as f32 / (packet.channels as u64 * packet.rate) as f32;
-        let packet_duration = Duration::milliseconds((packet_duration * 1000.0) as i64);
+        // adding to the queue
+        buffers.push((packet_play_finish, al::Buffer::gen()));
 
+        // feeding OpenAL with our buffer
         let format = match packet.channels {
             1 => al::FormatMono16,
             2 => al::FormatStereo16,
             _ => unimplemented!()
         };
 
-        unsafe { buffers.back_mut().unwrap()
+        unsafe { buffers.back_mut().unwrap().1
             .buffer_data(format, packet.data.as_slice(), packet.rate as al::ALsizei) };
-        source.queue_buffer(buffers.back().unwrap());
+        source.queue_buffer(&buffers.back().unwrap().1);
 
         if !source.is_playing() {
             source.play();
         }
 
-        if packet_num >= 2 {
-            sleep(previous_packet_duration);
+        // if we have more than 4 buffers in queue, we can start sleeping until the first one
+        // has finished playing
+        if buffers.len() >= 4 {
+            match buffers.front().map(|&(stop, _)| stop) {
+                Some(wakeup) => {
+                    let now = time::precise_time_ns();
+                    if wakeup > now {
+                        sleep(Duration::nanoseconds((wakeup - now) as i64));
+                    }
+                },
+                _ => ()
+            }
         }
 
-        previous_packet_duration = packet_duration;
+        println!("packet {}", packet_num);
     }
+
+    // sleeping until the last buffer has finished playing
+    match buffers.back().map(|&(stop, _)| stop) {
+        Some(wakeup) => {
+            let now = time::precise_time_ns();
+            sleep(Duration::nanoseconds((wakeup - now) as i64));
+        },
+        _ => ()
+    }
+
 
     ctx.destroy();
     device.close().ok().expect("Unable to close device");
